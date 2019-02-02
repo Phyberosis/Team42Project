10f755c15e10cc2c700ab37ddff1260e38331743
y0c1b <ContactTonyLiu@gmail.com>
2019-02-01 22:07:15 -0800

    Initial commit

---
+++ src/main/java/team42Application.java
@@ -0,0 +1,17 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class team42Application {
+    public static void main(String[] args) {
+
+        FileInputStream serviceAccount;
+        try {
+            serviceAccount = new FileInputStream("./ServiceAccountKey.json");
+        }catch(FileNotFoundException e){
+            System.out.println("bad service address");
+            System.exit(0);
+        }
+
+
+    }
+}
